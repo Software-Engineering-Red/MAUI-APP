01006a5d75c2c1394ceb19f40340db3c8e35fde5
Stefan Humpelstetter <38726293+batchgott@users.noreply.github.com>
2023-09-21 22:29:02 +0100

    Feature/ma UI app#2 (#20)
    * In this commit I have added taskbar functionality for a basic UI
    
    * Add in folders for views and models
    
    * Add in a way of viewing a role, and save and delete buttons
    
    * At a solid point
    
    * still
    
    * make changes
    
    ---------
    
    Co-authored-by: herrone <egretfulherron@gmail.com>

---
+++ Models/Welcome.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Models
+{
+    internal class Welcome
+    {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInfoUrl => "https://aka.ms/maui";
+        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+
+    }
+}
---
+++ Services/ContinentService.cs
@@ -0,0 +1,45 @@
+﻿using MauiApp1.Data;
+using MauiApp1.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services
+{
+    public class ContinentService : IContinentService {
+
+        private SQLiteAsyncConnection _dbConnection;
+
+        private async Task SetUpDb() {
+            if (_dbConnection != null)
+                return;
+            
+
+            _dbConnection = new SQLiteAsyncConnection(DatabaseSettings.DBPath, DatabaseSettings.Flags);
+            await _dbConnection.CreateTableAsync<Continent>();
+        }
+
+        public async Task<int> AddContinent(Continent continent) {
+            await SetUpDb();
+            return await _dbConnection.InsertAsync(continent);
+        }
+
+        public async Task<int> DeleteContinent(Continent continent) {
+            await SetUpDb();
+            return await _dbConnection.DeleteAsync(continent);
+        }
+
+        public async Task<List<Continent>> GetContinentList() {
+            await SetUpDb();
+            return await _dbConnection.Table<Continent>().ToListAsync();
+        }
+
+        public async Task<int> UpdateContinent(Continent continent) {
+            await SetUpDb();
+            return await _dbConnection.UpdateAsync(continent);
+        }
+    }
+}

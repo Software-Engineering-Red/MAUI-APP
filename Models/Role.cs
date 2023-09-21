d7287a18db86621c7c18d0cb32c667c126df7d9e
Brandon <brandon.streetsmcleod@gmail.com>
2023-09-21 21:04:34 +0100

    Created role class to allow for roles to be assigned

---
+++ Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiApp1.Models;
+public class Role
+{
+    [PrimaryKey, AutoIncrement]
+    public string Name { get; set; }
+}

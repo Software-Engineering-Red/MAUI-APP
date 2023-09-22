0a20f41e35146f8094332df7fc029be718ac27b7
herrone <egretfulherron@gmail.com>
2023-09-22 12:43:31 +0100

    Provide full working functionality for Roles, task #2

--- Services/RoleService.cs
+++ Services/RoleService.cs
@@ -9,35 +9,41 @@ using System.Threading.Tasks;
 
 namespace MauiApp1.Services
 {
-    public class RoleService : IRoleService {
+    public class RoleService : IRoleService
+    {
 
         private SQLiteAsyncConnection _dbConnection;
 
-        private async Task SetUpDb() {
+        private async Task SetUpDb()
+        {
             if (_dbConnection != null)
                 return;
-            
+
 
             _dbConnection = new SQLiteAsyncConnection(DatabaseSettings.DBPath, DatabaseSettings.Flags);
             await _dbConnection.CreateTableAsync<Role>();
         }
 
-        public async Task<int> AddRole(Role role) {
+        public async Task<int> AddRole(Role role)
+        {
             await SetUpDb();
             return await _dbConnection.InsertAsync(role);
         }
 
-        public async Task<int> DeleteRole(Role role) {
+        public async Task<int> DeleteRole(Role role)
+        {
             await SetUpDb();
             return await _dbConnection.DeleteAsync(role);
         }
 
-        public async Task<List<Role>> GetRoleList() {
+        public async Task<List<Role>> GetRoleList()
+        {
             await SetUpDb();
             return await _dbConnection.Table<Role>().ToListAsync();
         }
 
-        public async Task<int> UpdateRole(Role role) {
+        public async Task<int> UpdateRole(Role role)
+        {
             await SetUpDb();
             return await _dbConnection.UpdateAsync(role);
         }
--- Views/RolePage.xaml.cs
+++ Views/RolePage.xaml.cs
@@ -1,30 +1,76 @@
 ﻿
-using System.Reflection.Metadata;
+using MauiApp1.Models;
+using MauiApp1.Services;
+using System.Collections.ObjectModel;
 
 namespace MauiApp1.Views;
 
 public partial class RolePage : ContentPage
 {
-    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+    Role selectedRole = null;
+    IRoleService roleService;
+    ObservableCollection<Role> roles = new ObservableCollection<Role>();
+
     public RolePage()
     {
         InitializeComponent();
+        this.BindingContext = this;
+        this.roleService = new RoleService();
 
-        if (File.Exists(_fileName))
-            TextEditor.Text = File.ReadAllText(_fileName);
+        Task.Run(async () => await LoadRoles());
+        txe_role.Text = "";
     }
+
+    private async Task LoadRoles()
+    {
+        roles = new ObservableCollection<Role>(await roleService.GetRoleList());
+        ltv_roles.ItemsSource = roles;
+    }
+
     private void SaveButton_Clicked(object sender, EventArgs e)
     {
-        // Save the file.
-        File.WriteAllText(_fileName, TextEditor.Text);
+        if (String.IsNullOrEmpty(txe_role.Text)) return;
+
+        if (selectedRole == null)
+        {
+            var role = new Role() { Name = txe_role.Text };
+            roleService.AddRole(role);
+            roles.Add(role);
+        }
+        else
+        {
+            selectedRole.Name = txe_role.Text;
+            roleService.UpdateRole(selectedRole);
+            var role = roles.FirstOrDefault(x => x.ID == selectedRole.ID);
+            role.Name = txe_role.Text;
+        }
+
+
+        selectedRole = null;
+        ltv_roles.SelectedItem = null;
+        txe_role.Text = "";
     }
 
-    private void DeleteButton_Clicked(object sender, EventArgs e)
+    private async void DeleteButton_Clicked(object sender, EventArgs e)
     {
-        // Delete the file.
-        if (File.Exists(_fileName))
-            File.Delete(_fileName);
+        if (ltv_roles.SelectedItem == null)
+        {
+            await Shell.Current.DisplayAlert("No Role Selected", "Select the role you want to delete from the list", "OK");
+            return;
+        }
 
-        TextEditor.Text = string.Empty;
+        await roleService.DeleteRole(selectedRole);
+        roles.Remove(selectedRole);
+
+        ltv_roles.SelectedItem = null;
+        txe_role.Text = "";
+    }
+
+    private void ltv_roles_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        selectedRole = e.SelectedItem as Role;
+        if (selectedRole == null) return;
+
+        txe_role.Text = selectedRole.Name;
     }
 }

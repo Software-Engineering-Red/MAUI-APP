bb600d7254cc0242b766924d3a061596d4d21a02
Ryan Maybury <ryan2711@hotmail.co.uk>
2023-09-23 15:55:02 +0100

    Initial Commit

---
+++ Services/AlertTypeService.cs
@@ -0,0 +1,52 @@
+﻿using MauiApp1.Data;
+using MauiApp1.Models;
+using Microsoft.Maui.Animations;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services
+{
+    public class AlertTypeService : IAlertTypeService
+    {
+
+        private SQLiteAsyncConnection _dbConnection;
+
+        private async Task SetUpDb()
+        {
+            if (_dbConnection != null)
+                return;
+
+
+            _dbConnection = new SQLiteAsyncConnection(DatabaseSettings.DBPath, DatabaseSettings.Flags);
+            await _dbConnection.CreateTableAsync<AlertType>();
+        }
+
+        public async Task<int> AddAlertType(AlertType alert)
+        {
+            await SetUpDb();
+            return await _dbConnection.InsertAsync(alert);
+        }
+
+        public async Task<int> DeleteAlertType(AlertType alert)
+        {
+            await SetUpDb();
+            return await _dbConnection.DeleteAsync(alert);
+        }
+
+        public async Task<List<AlertType>> GetAlertTypes()
+        {
+            await SetUpDb();
+            return await _dbConnection.Table<AlertType>().ToListAsync();
+        }
+
+        public async Task<int> UpdateAlertType(AlertType alert)
+        {
+            await SetUpDb();
+            return await _dbConnection.UpdateAsync(alert);
+        }
+    }
+}
---
+++ Services/IAlertTypeService.cs
@@ -0,0 +1,17 @@
+﻿using MauiApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services
+{
+    public interface IAlertTypeService
+    {
+        Task<List<AlertType>> GetAlertTypes();
+        Task<int> AddAlertType(AlertType continent);
+        Task<int> DeleteAlertType(AlertType continent);
+        Task<int> UpdateAlertType(AlertType continent);
+    }
+}

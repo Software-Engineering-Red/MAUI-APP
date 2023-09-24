3bee6a6ead1f2b70e81bf56506d9f36c34cd06b5
Pannoniae <prisznyak.zsombor@gmail.com>
2023-09-24 21:47:17 +0100

    Feature/MAUI-APP#9 (#26)
    * - add building types and building types UI

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -24,6 +24,7 @@ public static class MauiProgram
 		builder.Services.AddSingleton<Database>();
         builder.Services.AddSingleton<IContinentService, ContinentService>();
         builder.Services.AddSingleton<ISkillService, SkillService>();
+        builder.Services.AddSingleton<IBuildingTypeService, BuildingTypeService>();
 
         return builder.Build();
 	}
---
+++ Services/BuildingTypeService.cs
@@ -0,0 +1,45 @@
+﻿using MauiApp1.Data;
+using MauiApp1.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services
+{
+    public class BuildingTypeService : IBuildingTypeService {
+
+        private SQLiteAsyncConnection _dbConnection;
+
+        private async Task SetUpDb() {
+            if (_dbConnection != null)
+                return;
+            
+
+            _dbConnection = new SQLiteAsyncConnection(DatabaseSettings.DBPath, DatabaseSettings.Flags);
+            await _dbConnection.CreateTableAsync<BuildingType>();
+        }
+
+        public async Task<int> AddBuildingType(BuildingType buildingType) {
+            await SetUpDb();
+            return await _dbConnection.InsertAsync(buildingType);
+        }
+
+        public async Task<int> DeleteBuildingType(BuildingType buildingType) {
+            await SetUpDb();
+            return await _dbConnection.DeleteAsync(buildingType);
+        }
+
+        public async Task<List<BuildingType>> GetBuildingTypeList() {
+            await SetUpDb();
+            return await _dbConnection.Table<BuildingType>().ToListAsync();
+        }
+
+        public async Task<int> UpdateBuildingType(BuildingType buildingType) {
+            await SetUpDb();
+            return await _dbConnection.UpdateAsync(buildingType);
+        }
+    }
+}

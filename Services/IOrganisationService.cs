300796f015d0e24c82a436dc5e02d7eb9d8c07a7
gjemiolo.git <jemiolo.g@gmail.com>
2023-09-24 22:16:44 +0100

    Add CRUD for organisation type, based on format from Stefan and Emily

---
+++ Services/IOrganisationService.cs
@@ -0,0 +1,12 @@
+﻿using MauiApp1.Models;
+
+namespace MauiApp1.Services
+{
+    public interface IOrganisationService
+    {
+        Task<List<Organisation>> GetOrganisationList();
+        Task<int> AddOrganisation(Organisation org);
+        Task<int> DeleteOrganisation(Organisation org);
+        Task<int> UpdateOrganisation(Organisation org);
+    }
+}

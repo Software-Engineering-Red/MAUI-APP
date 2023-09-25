83eae04917f1ff2eb26427fc0962bac4891c96f5
Calvin Donaldson <calvindonaldson1997313@gmail.com>
2023-09-25 17:33:32 +0100

    Feature/MAUI-APP#7 Added Room types

---
+++ Services/RoomTypeService.cs
@@ -0,0 +1,53 @@
+﻿using MauiApp1.Data;
+using MauiApp1.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services
+{
+    public class RoomTypeService : IRoomTypeService
+    {
+
+        private SQLiteAsyncConnection _dbConnection;
+
+        private async Task SetUpDb()
+        {
+            if (_dbConnection != null)
+                return;
+
+
+            _dbConnection = new SQLiteAsyncConnection(DatabaseSettings.DBPath, DatabaseSettings.Flags);
+            await _dbConnection.CreateTableAsync<RoomType>();
+        }
+
+        public async Task<int> AddRoomType(RoomType roomType)
+        {
+            await SetUpDb();
+            return await _dbConnection.InsertAsync(roomType);
+        }
+
+        public async Task<int> DeleteRoomType(RoomType roomType)
+        {
+            await SetUpDb();
+            return await _dbConnection.DeleteAsync(roomType);
+        }
+
+        public async Task<List<RoomType>> GetRoomTypeList()
+        {
+            await SetUpDb();
+            return await _dbConnection.Table<RoomType>().ToListAsync();
+        }
+
+        public async Task<int> UpdateRoomType(RoomType roomType)
+        {
+            await SetUpDb();
+            return await _dbConnection.UpdateAsync(roomType);
+        }
+
+        
+    }
+}

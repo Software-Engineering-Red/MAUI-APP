33d89f183f744085593042db34f80317f6a97b30
GLefterov <lefterov01@gmail.com>
2023-10-05 17:00:06 +0300

    Added some descriptive doxygen comments

--- Views/EquipmentPage.xaml.cs
+++ Views/EquipmentPage.xaml.cs
@@ -4,29 +4,49 @@ using MauiApp1.Services;
 using System.Collections.ObjectModel;
 
 namespace MauiApp1.Views;
-
+/*! <summary>
+        EquipmentPage class extending ContentPage, responsible for functionality on EquipmentPage view.
+    </summary> */
 public partial class EquipmentPage : ContentPage
 {
+    /*! <summary>
+        A reference pointer for storing currently selected Equipment.
+     </summary> */
     Equipment selectedEquipment = null;
+    /*! <summary>
+        An instance of IEquipmentService
+     </summary> */
     IEquipmentService equipmentService;
+    /*! <summary>
+        Collection of current Equipments.
+    </summary> */
     ObservableCollection<Equipment> equipments = new ObservableCollection<Equipment>();
-
+    /*! <summary>
+        Constructor class, setting the binding context and initiating the equipment service, as well as loading the equipment list.
+    </summary> */
     public EquipmentPage()
     {
         InitializeComponent();
         this.BindingContext = this;
         this.equipmentService = new EquipmentService();
 
-        Task.Run(async () => await LoadRoles());
+        Task.Run(async () => await LoadEquipment());
         txe_equipment.Text = "";
     }
-
-    private async Task LoadRoles()
+    /*! <summary>
+        Private method loading the Equipment list using equipmentService getter.
+    </summary> 
+    <returns>Task promise, informing about the status of its' completion.</returns> */
+    private async Task LoadEquipment()
     {
         equipments = new ObservableCollection<Equipment>(await equipmentService.GetEquipmentList());
         ltv_equipment.ItemsSource = equipments;
     }
-
+    /*! <summary>
+         Method responsible for saving equipment into SQLite database, triggered by selection of save button.
+     </summary> 
+     <param name="sender">Details about the element that triggered the event.</param>
+     <param name="e">Event details, passed by eventHandler due to clicking event button.</param> */
     private void SaveButton_Clicked(object sender, EventArgs e)
     {
         if (String.IsNullOrEmpty(txe_equipment.Text)) return;
@@ -50,7 +70,12 @@ public partial class EquipmentPage : ContentPage
         ltv_equipment.SelectedItem = null;
         txe_equipment.Text = "";
     }
-
+    /*! <summary>
+         Method responsible for removing organisation from SQLite database, triggered by selection of delete button.
+         Note: If no Organisation is selected, no organisation will be removed.
+    </summary> 
+    <param name="sender">Details about the element that triggered the event.</param>
+    <param name="e">Event details, passed by eventHandler due to clicking event button.</param> */
     private async void DeleteButton_Clicked(object sender, EventArgs e)
     {
         if (ltv_equipment.SelectedItem == null)
@@ -66,6 +91,11 @@ public partial class EquipmentPage : ContentPage
         txe_equipment.Text = "";
     }
 
+    /*! <summary>
+            Method responsible for updating currently selected item, integrating UI and Backend functionality.
+        </summary> 
+        <param name="sender">Details about the element that triggered the event.</param>
+        <param name="e">Event details, passed by eventHandler due to clicking event button.</param> */
     private void ltv_equipment_ItemSelected(object sender, SelectedItemChangedEventArgs e)
     {
         selectedEquipment = e.SelectedItem as Equipment;

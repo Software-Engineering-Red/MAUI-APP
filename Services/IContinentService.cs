5803b6f62c09103f7750fb6520af5a8e66473eb6
Stefan Humpelstetter <shumpelstetter@gmail.com>
2023-10-06 18:44:00 +0100

    Add Doxygen comments

--- Services/IContinentService.cs
+++ Services/IContinentService.cs
@@ -7,11 +7,36 @@ using System.Threading.Tasks;
 
 namespace MauiApp1.Services
 {
+    /*! <summary>
+        Interface that exposes methods of ContinentService
+    </summary> */
     public interface IContinentService
     {
+        /*! <summary>
+         Method responsible for quering database, for entries in Continent table
+        </summary> 
+         <returns>Returns Task containing List of Continents present in the database.</returns>*/
         Task<List<Continent>> GetContinentList();
+
+        /*! <summary>
+            Method responsible for addtion of entry into Continent table.
+        </summary> 
+         <param name="org">Continent class instance that is attempted to be inserted into Continent table.</param>
+         <returns>Returns Task containing number of rows inserted into Continent table.</returns> */
         Task<int> AddContinent(Continent continent);
+
+        /*! <summary>
+         Method responsible for deletion of an entry from Continent table.
+        </summary> 
+         <param name="org">Continent class instance, that is attempted to be deleted from Continent table. (Based on its' primary key)</param>
+         <returns>Returns Task containing number of rows deleted from Continent table.</returns>*/
         Task<int> DeleteContinent(Continent continent);
+
+        /*! <summary>
+         Method responsible for updating an entry from Continent table, based on primary key of the element passed.
+        </summary> 
+         <param name="org">Continent class instance that will be attempted to be updated, based on its' primary key value</param>
+         <returns>Returns Task containing number of rows updated in Continent table.</returns>*/
         Task<int> UpdateContinent(Continent continent);
     }
 }
--- Views/ContinentPage.xaml.cs
+++ Views/ContinentPage.xaml.cs
@@ -5,11 +5,28 @@ using System.Collections.ObjectModel;
 
 namespace MauiApp1.Views;
 
+/*! <summary>
+        ContinentPage class extending ContentPage, responsible for functionality on ContinentPage view.
+    </summary> */
 public partial class ContinentPage : ContentPage {
+    /*! <summary>
+        A reference pointer for storing currently selected Continent.
+     </summary> */
     Continent selectedContinent = null;
+
+    /*! <summary>
+        An instance of IContinentService
+     </summary> */
     IContinentService continentService;
+
+    /*! <summary>
+        Collection of current Continents.
+    </summary> */
     ObservableCollection<Continent> continents = new ObservableCollection<Continent>();
 
+    /*! <summary>
+        Constructor class, setting the binding context and initiating the Continent serrvice, as well as loading the Continent list.
+    </summary> */
     public ContinentPage() {
         InitializeComponent();
         this.BindingContext = this;
@@ -19,11 +36,20 @@ public partial class ContinentPage : ContentPage {
         txe_continent.Text = "";
     }
 
+    /*! <summary>
+            Private method loading the Continent list using ContinentService getter.
+        </summary> 
+        <returns>Task promise, informing about the status of its' completion.</returns> */
     private async Task LoadContinents() {
         continents = new ObservableCollection<Continent>(await continentService.GetContinentList());
         ltv_continents.ItemsSource = continents;
     }
 
+    /*! <summary>
+            Method responsible for saving Continent into SQLite database, triggered by selection of save button.
+        </summary> 
+        <param name="sender">Details about the element that triggered the event.</param>
+        <param name="e">Event details, passed by eventHandler due to clicking event button.</param> */
     private void SaveButton_Clicked(object sender, EventArgs e) {
         if (String.IsNullOrEmpty(txe_continent.Text)) return;
 
@@ -44,6 +70,12 @@ public partial class ContinentPage : ContentPage {
         txe_continent.Text = "";
     }
 
+    /*! <summary>
+             Method responsible for removing Continent from SQLite database, triggered by selection of delete button.
+             Note: If no Continent is selected, no Continent will be removed.
+        </summary> 
+        <param name="sender">Details about the element that triggered the event.</param>
+        <param name="e">Event details, passed by eventHandler due to clicking event button.</param> */
     private async void DeleteButton_Clicked(object sender, EventArgs e) {
         if(ltv_continents.SelectedItem == null) {
             await Shell.Current.DisplayAlert("No Continent Selected", "Select the continent you want to delete from the list", "OK");
@@ -57,6 +89,11 @@ public partial class ContinentPage : ContentPage {
         txe_continent.Text = "";
     }
 
+    /*! <summary>
+            Method responsible for updating currently selected item, integrating UI and Backend functionality.
+        </summary> 
+        <param name="sender">Details about the element that triggered the event.</param>
+        <param name="e">Event details, passed by eventHandler due to clicking event button.</param> */
     private void ltv_continents_ItemSelected(object sender, SelectedItemChangedEventArgs e) {
         selectedContinent = e.SelectedItem as Continent;
         if (selectedContinent == null) return;

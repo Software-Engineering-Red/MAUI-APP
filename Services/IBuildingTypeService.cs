58db1640e3b6137261757463ec4402acc1891239
prisz <25565223+Pannoniae@users.noreply.github.com>
2023-10-06 23:39:49 +0100

    documentation for buildingtypes

--- Services/IBuildingTypeService.cs
+++ Services/IBuildingTypeService.cs
@@ -7,11 +7,33 @@ using System.Threading.Tasks;
 
 namespace MauiApp1.Services
 {
+    /// <summary>
+    /// A service for handling building types.
+    /// </summary>
     public interface IBuildingTypeService
     {
+        /// <summary>
+        /// Gets a list of available building types.
+        /// </summary>
+        /// <returns>The list of available building types.</returns>
         Task<List<BuildingType>> GetBuildingTypeList();
+
+        /// <summary>
+        /// Add a building type.
+        /// </summary>
+        /// <param name="buildingType">The building type to be added.</param>
         Task<int> AddBuildingType(BuildingType buildingType);
+
+        /// <summary>
+        /// Delete a building type,
+        /// </summary>
+        /// <param name="buildingType">The building type to be deleted.</param>
         Task<int> DeleteBuildingType(BuildingType buildingType);
+
+        /// <summary>
+        /// Update a building type.
+        /// </summary>
+        /// <param name="buildingType">The building type to be updated.</param>
         Task<int> UpdateBuildingType(BuildingType buildingType);
     }
 }

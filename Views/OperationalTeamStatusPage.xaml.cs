e2f8fc8e6aa1b77e63f0d901bf7bc3795151c3fe
byVirrox <116073359+byVirrox@users.noreply.github.com>
2023-10-07 10:53:04 +0100

    commented OperationalTeamStatusPage

--- Views/OperationalTeamStatusPage.xaml.cs
+++ Views/OperationalTeamStatusPage.xaml.cs
@@ -4,13 +4,31 @@ using System.Collections.ObjectModel;
 
 namespace MauiApp1.Views;
 
+/// <summary>
+/// This class extending ContentPage provides the functional logic to
+/// the User interface to interact with the Model.
+/// </summary>
 public partial class OperationalTeamStatusPage : ContentPage
 {
-
+    /// <summary>
+    /// currently selected instance of OperationalTeamStatus.
+    /// </summary>
     OperationalTeamStatus selectedStatus = null;
+
+    /// <summary>
+    /// Instance of IOperationalTeamStatusService Interface.
+    /// </summary>
     IOperationalTeamStatusService statusService;
+
+    /// <summary>
+    /// Collection of of all current instances of OperationalTeamStatus.
+    /// </summary>
     ObservableCollection<OperationalTeamStatus> statuses = new ObservableCollection<OperationalTeamStatus>();
 
+    /// <summary>
+    /// The Constructor, initializes the Component, sets instance variables
+    /// and loads instances of OperationalTeamStatus from the database.
+    /// </summary>
     public OperationalTeamStatusPage()
     {
         InitializeComponent();
@@ -21,12 +39,23 @@ public partial class OperationalTeamStatusPage : ContentPage
         txe_status.Text = "";
     }
 
+    /// <summary>
+    /// Loads a OperationalTeamStatus Collection from the database
+    /// and reference the Collection to the in UI.
+    /// </summary>
+    /// <returns> Task promise</returns>
     private async Task LoadStatuses()
     {
         statuses = new ObservableCollection<OperationalTeamStatus>(await statusService.GetStatusesListAsync());
         ltv_statuses.ItemsSource = statuses;
     }
 
+    /// <summary>
+    /// Saves current Element of OperationalTeamStatus to Database
+    /// through a clicking save button in the UI.
+    /// </summary>
+    /// <param name="sender">Object to be saved</param>
+    /// <param name="e">UI Event triggering Method call </param>
     private void SaveButton_Clicked(object sender, EventArgs e)
     {
         if (String.IsNullOrEmpty(txe_status.Text)) return;
@@ -51,6 +80,11 @@ public partial class OperationalTeamStatusPage : ContentPage
         txe_status.Text = "";
     }
 
+    /// <summary>
+    /// Deletes current Element from the Database
+    /// </summary>
+    /// <param name="sender">Object to be deleted</param>
+    /// <param name="e">UI Event triggering Method call</param>
     private async void DeleteButton_Clicked(object sender, EventArgs e)
     {
         if (ltv_statuses.SelectedItem == null)
@@ -66,6 +100,11 @@ public partial class OperationalTeamStatusPage : ContentPage
         txe_status.Text = "";
     }
 
+    /// <summary>
+    /// Method to update currently selected OperationalTeamStatus
+    /// </summary>
+    /// <param name="sender">Object to be updated</param>
+    /// <param name="e">UI Event triggering Method call</param>
     private void ltv_status_ItemSelected(object sender, SelectedItemChangedEventArgs e)
     {
         selectedStatus = e.SelectedItem as OperationalTeamStatus;

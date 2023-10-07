d7e65691384a230b81a0162959d1a1a14365479f
Georgi Lefterov <46247795+GLefterov@users.noreply.github.com>
2023-10-07 19:30:33 +0300

    fixed the colour scheme of the app, fixed some bugs

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,7 +5,7 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-        Current.UserAppTheme = AppTheme.Dark;
+        Current.UserAppTheme = AppTheme.Light;
         MainPage = new AppShell();
 	}
 }

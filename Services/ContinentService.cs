66bd5d36dcfa7a93370694d013af576a448be0cc
Stefan Humpelstetter <38726293+batchgott@users.noreply.github.com>
2023-10-11 12:17:16 +0100

    MAUI-APP#50: Add Doxygen comments for Continents (#51)
    * Add Doxygen comments
    
    * test

--- Services/ContinentService.cs
+++ Services/ContinentService.cs
@@ -9,11 +9,19 @@ using System.Threading.Tasks;
 
 namespace MauiApp1.Services
 {
+    /*! <summary>
+        ContinentService extending IContinentService Interface
+    </summary> */
     public class ContinentService : IContinentService
     {
-
+        /*! <summary>
+            Variable storing dbConnection to SQLite database.
+        </summary> */
         private SQLiteAsyncConnection _dbConnection;
 
+        /*! <summary>
+            Method that initiates connection to SQLite database, and creates Continent class table, if none is present.
+        </summary> */
         private async Task SetUpDb() {
             if (_dbConnection != null)
                 return;
@@ -23,22 +31,41 @@ namespace MauiApp1.Services
             await _dbConnection.CreateTableAsync<Continent>();
         }
 
+        /*! <summary>
+            Method responsible for addtion of entry into Continent table.
+        </summary> 
+         <param name="org">Continent class instance that is attempted to be inserted into Continent table.</param>
+         <returns>Returns Task containing number of rows inserted into Continent table.</returns> */
         public async Task<int> AddContinent(Continent continent) {
             await SetUpDb();
             return await _dbConnection.InsertAsync(continent);
              
         }
 
+        /*! <summary>
+         Method responsible for deletion of an entry from Continent table.
+        </summary> 
+         <param name="org">Continent class instance, that is attempted to be deleted from Continent table. (Based on its' primary key)</param>
+         <returns>Returns Task containing number of rows deleted from Continent table.</returns>*/
         public async Task<int> DeleteContinent(Continent continent) {
             await SetUpDb();
             return await _dbConnection.DeleteAsync(continent);
         }
 
+        /*! <summary>
+         Method responsible for quering database, for entries in Continent table
+        </summary> 
+         <returns>Returns Task containing List of Continents present in the database.</returns>*/
         public async Task<List<Continent>> GetContinentList() {
             await SetUpDb();
             return await _dbConnection.Table<Continent>().ToListAsync();
         }
 
+        /*! <summary>
+         Method responsible for updating an entry from Continent table, based on primary key of the element passed.
+        </summary> 
+         <param name="org">Continent class instance that will be attempted to be updated, based on its' primary key value</param>
+         <returns>Returns Task containing number of rows updated in Continent table.</returns>*/
         public async Task<int> UpdateContinent(Continent continent) {
             await SetUpDb();
             return await _dbConnection.UpdateAsync(continent);

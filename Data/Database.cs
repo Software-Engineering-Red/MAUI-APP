8ecdde9694bbd8e61a5a52345556dd62e61e8d2d
herrone <egretfulherron@gmail.com>
2023-10-29 17:08:56 +0000

    Added volunteer functionality

---
+++ Data/Database.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Data
+{
+    public class Database
+    {
+        public SQLiteAsyncConnection _database;
+
+        async Task Init()
+        {
+            if (_database is not null)
+                return;
+
+            _database = new SQLiteAsyncConnection(DatabaseSettings.DBPath, DatabaseSettings.Flags);
+            // TODO: Create tables that we will use for project
+        }
+    }
+}

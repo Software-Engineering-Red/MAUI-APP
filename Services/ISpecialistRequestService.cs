5119498637127f97f15d2d711bbde9782f31b4c1
Peter Stibitz <peter.stibitz@kabelmail.de>
2023-10-29 14:22:05 +0000

    added namespace Models to services

--- Services/ISpecialistRequestService.cs
+++ Services/ISpecialistRequestService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MauiApp1.Models;
 
 namespace MauiApp1.Services
 {
--- Services/SpecialistRequestService.cs
+++ Services/SpecialistRequestService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MauiApp1.Models;
 
 namespace MauiApp1.Services
 {

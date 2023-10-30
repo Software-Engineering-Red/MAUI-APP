a23c25962b63b5381b4e142a07f1c7ba1a83699a
Stefan Humpelstetter <shumpelstetter@gmail.com>
2023-10-30 08:01:08 +0000

    push

---
+++ Models/Team.cs
@@ -0,0 +1,56 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Models
+{
+    internal class Team : INotifyPropertyChanged
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        private int maxSize;
+        public int MaxSize
+        {
+            get => maxSize;
+            set => SetField(ref maxSize, value);
+        }
+
+        private string skillsRequired;
+        public string SkillsRequired
+        {
+            get => skillsRequired;
+            set => SetField(ref skillsRequired, value);
+        }
+
+        private DateTime startDate;
+        public DateTime StartDate
+        {
+            get => startDate;
+            set => SetField(ref startDate, value);
+        }
+
+        private DateTime endDate;
+        public DateTime EndDate
+        {
+            get => endDate;
+            set => SetField(ref endDate, value);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
---
+++ Models/TeamMember.cs
@@ -0,0 +1,42 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Models
+{
+    public class TeamMember : INotifyPropertyChanged
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        private string firstname;
+        public string Firstname
+        {
+            get => firstname;
+            set => SetField(ref firstname, value);
+        }
+
+        private string lastname;
+        public string Lastname
+        {
+            get => lastname;
+            set => SetField(ref lastname, value);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}

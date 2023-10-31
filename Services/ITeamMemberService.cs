770519aaeed1f6c2497029de4e81d21d25a834b1
Brandon <brandon.streetsmcleod@gmail.com>
2023-10-31 11:23:45 +0000

    Added team member classes

---
+++ Services/ITeamMemberService.cs
@@ -0,0 +1,39 @@
+﻿using MauiApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services
+{
+    public interface ITeamMemberService
+    {
+        /*! <summary>
+         Method responsible for quering database, for entries in TeamMember table
+        </summary> 
+         <returns>Returns Task containing List of TeamMembers present in the database.</returns>*/
+        Task<List<TeamMember>> GetTeamMemberList();
+
+        /*! <summary>
+            Method responsible for addtion of entry into TeamMember table.
+        </summary> 
+         <param name="org">TeamMember class instance that is attempted to be inserted into TeamMember table.</param>
+         <returns>Returns Task containing number of rows inserted into TeamMember table.</returns> */
+        Task<int> AddTeamMember(TeamMember team_member);
+
+        /*! <summary>
+         Method responsible for deletion of an entry from TeamMember table.
+        </summary> 
+         <param name="org">TeamMember class instance, that is attempted to be deleted from TeamMember table. (Based on its' primary key)</param>
+         <returns>Returns Task containing number of rows deleted from TeamMember table.</returns>*/
+        Task<int> DeleteTeamMember(TeamMember team_member);
+
+        /*! <summary>
+         Method responsible for updating an entry from TeamMember table, based on primary key of the element passed.
+        </summary> 
+         <param name="org">TeamMember class instance that will be attempted to be updated, based on its' primary key value</param>
+         <returns>Returns Task containing number of rows updated in TeamMember table.</returns>*/
+        Task<int> UpdateTeamMember(TeamMember team_member);
+    }
+}
---
+++ Views/TeamMemberPage.xaml.cs
@@ -0,0 +1,109 @@
+using MauiApp1.Models;
+using MauiApp1.Services;
+using System.Collections.ObjectModel;
+
+namespace MauiApp1.Views;
+
+public partial class TeamMemberPage : ContentPage
+{
+    /*! <summary>
+        A reference pointer for storing currently selected TeamMember.
+     </summary> */
+    private TeamMember selectedTeamMember = null;
+
+    /*! <summary>
+        An instance of ITeamMemberService
+     </summary> */
+    ITeamMemberService teamMemberService;
+
+    /*! <summary>
+        Collection of current TeamMembers.
+    </summary> */
+    ObservableCollection<TeamMember> teamMembers = new ObservableCollection<TeamMember>();
+
+    /*! <summary>
+        Constructor class, setting the binding context and initiating the TeamMember serrvice, as well as loading the TeamMember list.
+    </summary> */
+    public TeamMemberPage()
+    {
+        InitializeComponent();
+        this.BindingContext = this;
+        this.teamMemberService = new TeamMemberService();
+
+        Task.Run(async () => await LoadTeamMembers());
+        txe_teamMember.Text = "";
+    }
+
+    /*! <summary>
+            Private method loading the TeamMember list using TeamMemberService getter.
+        </summary> 
+        <returns>Task promise, informing about the status of its' completion.</returns> */
+    private async Task LoadTeamMembers()
+    {
+        teamMembers = new ObservableCollection<TeamMember>(await teamMemberService.GetTeamMemberList());
+        ltv_teamMembers.ItemsSource = teamMembers;
+    }
+
+    /*! <summary>
+            Method responsible for saving TeamMember into SQLite database, triggered by selection of save button.
+        </summary> 
+        <param name="sender">Details about the element that triggered the event.</param>
+        <param name="e">Event details, passed by eventHandler due to clicking event button.</param> */
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        if (String.IsNullOrEmpty(txe_teamMember.Text)) return;
+
+        if (selectedTeamMember == null)
+        {
+            var teamMember = new TeamMember() { Name = txe_teamMember.Text };
+            teamMemberService.AddTeamMember(teamMember);
+            teamMembers.Add(teamMember);
+        }
+        else
+        {
+            selectedTeamMember.Name = txe_teamMember.Text;
+            teamMemberService.UpdateTeamMember(selectedTeamMember);
+            var teamMember = teamMembers.FirstOrDefault(x => x.ID == selectedTeamMember.ID);
+            teamMember.Name = txe_teamMember.Text;
+        }
+
+
+        selectedTeamMember = null;
+        ltv_teamMembers.SelectedItem = null;
+        txe_teamMember.Text = "";
+    }
+
+    /*! <summary>
+             Method responsible for removing TeamMember from SQLite database, triggered by selection of delete button.
+             Note: If no TeamMember is selected, no TeamMember will be removed.
+        </summary> 
+        <param name="sender">Details about the element that triggered the event.</param>
+        <param name="e">Event details, passed by eventHandler due to clicking event button.</param> */
+    private async void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        if (ltv_teamMembers.SelectedItem == null)
+        {
+            await Shell.Current.DisplayAlert("No TeamMember Selected", "Select the teamMember you want to delete from the list", "OK");
+            return;
+        }
+
+        await teamMemberService.DeleteTeamMember(selectedTeamMember);
+        teamMembers.Remove(selectedTeamMember);
+
+        ltv_teamMembers.SelectedItem = null;
+        txe_teamMember.Text = "";
+    }
+
+    /*! <summary>
+            Method responsible for updating currently selected item, integrating UI and Backend functionality.
+        </summary> 
+        <param name="sender">Details about the element that triggered the event.</param>
+        <param name="e">Event details, passed by eventHandler due to clicking event button.</param> */
+    private void ltv_teamMembers_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        selectedTeamMember = e.SelectedItem as TeamMember;
+        if (selectedTeamMember == null) return;
+
+        txe_teamMember.Text = selectedTeamMember.Name;
+    }
+}

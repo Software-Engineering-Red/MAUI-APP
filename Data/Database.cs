3a702cf71b1e051bd53a1b4a1fde4ad006368624
Emesaye <k.pajak.pjx@gmail.com>
2023-11-02 01:20:43 +0000

    FIXED THE DATABASE ERRORS

--- Data/Database.cs
+++ Data/Database.cs
@@ -10,14 +10,42 @@ namespace MauiApp1.Data
     public class Database
     {
         public SQLiteAsyncConnection _database;
+        public Database()
+        {
+            InitializeDatabaseAsync().Wait(); // Ensure initialization is complete before proceeding
+        }
 
-        async Task Init()
+        public async Task InitializeDatabaseAsync()
         {
             if (_database is not null)
                 return;
 
-            _database = new SQLiteAsyncConnection(DatabaseSettings.DBPath, DatabaseSettings.Flags);
-            // TODO: Create tables that we will use for project
+            try
+            {
+                _database = new SQLiteAsyncConnection(DatabaseSettings.DBPath, DatabaseSettings.Flags);
+                await CreateTablesAsync();
+            }
+            catch (Exception ex)
+            {
+                // Handle any initialization errors here, e.g., log or display an error message.
+                Console.WriteLine($"Database initialization error: {ex.Message}");
+            }
+        }
+        public async Task CreateTablesAsync()
+        {
+            try
+            {
+                // Create your database tables here
+                await _database.CreateTableAsync<Models.AlertType>();
+                await _database.CreateTableAsync<Models.OperationalTeamStatus>();
+
+                // Add more CreateTableAsync calls for additional tables as needed.
+            }
+            catch (Exception ex)
+            {
+                // Handle any table creation errors here, e.g., log or display an error message.
+                Console.WriteLine($"Table creation error: {ex.Message}");
+            }
         }
     }
 }

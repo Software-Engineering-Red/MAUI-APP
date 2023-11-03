863b14b4662edd3794da30e2cb084c357f8041ce
Konrad Pajak <40429087@live.napier.ac.uk>
2023-11-03 09:51:14 +0000

    added comments to one of model classes, all of them should be the same

--- Models/BuildingType.cs
+++ Models/BuildingType.cs
@@ -21,15 +21,20 @@ namespace MauiApp1.Models
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
 
-
+        // A public property for an integer ID.
         private string _name;
+        // A private field to store the value of the Name property.
+
         public string Name
         {
+            // Getter returns the value of the private _name field.
             get => _name;
+            // Setter uses SetProperty to update the value and raise PropertyChanged event.
             set => SetProperty(ref _name, value);
         }
-
+        // Event to notify subscribers when a property changes.
         public event PropertyChangedEventHandler? PropertyChanged;
+        // Method to raise the PropertyChanged event with the property name.
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
@@ -37,14 +42,15 @@ namespace MauiApp1.Models
 
         protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
         {
+            // If the new value is the same as the current value, no change is made, and false is returned.
             if (EqualityComparer<T>.Default.Equals(storage, value))
             {
                 return false;
             }
 
-            storage = value;
-            OnPropertyChanged(propertyName);
-            return true;
+            storage = value; // Update the field with the new value.
+            OnPropertyChanged(propertyName);// Notify subscribers that the property has changed.
+            return true; // Return true to indicate that the property was changed.
         }
     }
 }

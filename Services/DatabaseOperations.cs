0e71c00b5a4abf06d2694aa4521c27b98626bb81
Georgi Lefterov <46247795+GLefterov@users.noreply.github.com>
2023-11-04 19:03:45 +0200

    moved functionality and added services to the repo

---
+++ Services/DatabaseOperations.cs
@@ -0,0 +1,119 @@
+﻿using MauiApp1.Models;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services
+{
+    public class DatabaseOperations
+    {
+        private readonly string _dbPath;
+
+        public DatabaseOperations(string dbPath)
+        {
+            _dbPath = dbPath;
+            InitializeDatabase().Wait();
+        }
+
+        private async Task InitializeDatabase()
+        {
+            using (var connection = new SqliteConnection($"Data Source={_dbPath}"))
+            {
+                await connection.OpenAsync();
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = @"CREATE TABLE IF NOT EXISTS Pins (
+                                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                        Name TEXT,
+                                        Latitude REAL,
+                                        Longitude REAL,
+                                        PinType TEXT,
+                                        StatusOrPurpose TEXT
+                                    )";
+                await cmd.ExecuteNonQueryAsync();
+            }
+        }
+
+        public List<CustomPin> GetAllPins()
+        {
+            var pins = new List<CustomPin>();
+
+            using (var connection = new SqliteConnection($"Data Source={_dbPath}"))
+            {
+                connection.Open();
+
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT Id, Name, Latitude, Longitude, PinType, StatusOrPurpose FROM Pins";
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        pins.Add(new CustomPin
+                        {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Latitude = reader.GetDouble(2),
+                            Longitude = reader.GetDouble(3),
+                            PinType = reader.GetString(4),
+                            StatusOrPurpose = reader.GetString(5)
+                        });
+                    }
+                }
+            }
+
+            return pins;
+        }
+
+        public async Task InsertPinAsync(CustomPin pin)
+        {
+            using (var connection = new SqliteConnection($"Data Source={_dbPath}"))
+            {
+                await connection.OpenAsync();
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = @"INSERT INTO Pins (Name, Latitude, Longitude, PinType, StatusOrPurpose) 
+                                    VALUES (@Name, @Latitude, @Longitude, @PinType, @StatusOrPurpose)";
+                cmd.Parameters.AddWithValue("@Name", pin.Name);
+                cmd.Parameters.AddWithValue("@Latitude", pin.Latitude);
+                cmd.Parameters.AddWithValue("@Longitude", pin.Longitude);
+                cmd.Parameters.AddWithValue("@PinType", pin.PinType);
+                cmd.Parameters.AddWithValue("@StatusOrPurpose", pin.StatusOrPurpose);
+
+                await cmd.ExecuteNonQueryAsync();
+            }
+        }
+
+        public async Task UpdatePinAsync(CustomPin pin)
+        {
+            using (var connection = new SqliteConnection($"Data Source={_dbPath}"))
+            {
+                await connection.OpenAsync();
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = @"UPDATE Pins SET Name = @Name, Latitude = @Latitude, Longitude = @Longitude, 
+                                    PinType = @PinType, StatusOrPurpose = @StatusOrPurpose WHERE Id = @Id";
+                cmd.Parameters.AddWithValue("@Id", pin.Id);
+                cmd.Parameters.AddWithValue("@Name", pin.Name);
+                cmd.Parameters.AddWithValue("@Latitude", pin.Latitude);
+                cmd.Parameters.AddWithValue("@Longitude", pin.Longitude);
+                cmd.Parameters.AddWithValue("@PinType", pin.PinType);
+                cmd.Parameters.AddWithValue("@StatusOrPurpose", pin.StatusOrPurpose);
+
+                await cmd.ExecuteNonQueryAsync();
+            }
+        }
+
+        public async Task DeletePinAsync(CustomPin pin)
+        {
+            using (var connection = new SqliteConnection($"Data Source={_dbPath}"))
+            {
+                await connection.OpenAsync();
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = @"DELETE FROM Pins WHERE Id = @Id";
+                cmd.Parameters.AddWithValue("@Id", pin.Id);
+
+                await cmd.ExecuteNonQueryAsync();
+            }
+        }
+    }
+}
---
+++ Services/MapService.cs
@@ -0,0 +1,45 @@
+﻿using MauiApp1.Models;
+using Microsoft.Maui.Controls.Maps;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MauiApp1.Services
+{
+    public class MapService
+    {
+        private readonly Map _map;
+        private readonly DatabaseOperations _dbOperations;
+
+        public MapService(Map map)
+        {
+            _map = map;
+            _dbOperations = new DatabaseOperations(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "operation_map.db"));
+            LoadPins();
+        }
+
+        private void LoadPins()
+        {
+            var pinsFromDb = _dbOperations.GetAllPins();
+            foreach (var pin in pinsFromDb)
+            {
+                AddPinToMap(pin);
+            }
+        }
+
+        public void AddPinToMap(CustomPin pin)
+        {
+            var mapPin = new Pin
+            {
+                Label = pin.Name,
+                Position = new Position(pin.Latitude, pin.Longitude),
+                Address = pin.StatusOrPurpose,  // Assuming StatusOrPurpose contains address-like info
+                Type = PinType.SavedPin
+            };
+            // Set the type based on PinType if needed, you can adjust the logic here.
+
+            _map.Pins.Add(mapPin);
+        }
+
+        // Add methods to toggle visibility, handle zoom, etc.
+    }
+}

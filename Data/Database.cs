8c1db9ca21efb7b560b08b3832b18fb233a4e6f0
kristynrogan <bodfrey2195@hotmail.com>
2023-11-05 23:23:46 +0000

    database changes established

--- Data/Database.cs
+++ Data/Database.cs
@@ -1,61 +1,66 @@
-﻿using SQLite;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace MauiApp1.Data
 {
     public class Database
     {
-        public SQLiteAsyncConnection _database;
-        public Database()
+        private readonly SQLiteAsyncConnection _database;
+        private bool _isInitialised;
+
+        public Database(string dbPath)
         {
-            InitializeDatabaseAsync().Wait(); // Ensure initialization is complete before proceeding
+            // Confirm that the database path is neither null nor empty as this is a critical requirement.
+            if (string.IsNullOrEmpty(dbPath))
+            {
+                throw new ArgumentException("The database path must not be null or empty.", nameof(dbPath));
+            }
+
+            // Set up the SQLite connection using the given path.
+            _database = new SQLiteAsyncConnection(dbPath);
         }
 
-        public async Task InitializeDatabaseAsync()
+        public async Task InitialiseAsync()
         {
-            if (_database is not null)
+            // If the database has already been set up, there's no need to repeat the process.
+            if (_isInitialised)
+            {
                 return;
+            }
 
-            try
-            {
-                _database = new SQLiteAsyncConnection(DatabaseSettings.DBPath, DatabaseSettings.Flags);
-                await CreateTablesAsync();
-            }
-            catch (Exception ex)
-            {
-                // Handle any initialization errors here, e.g., log or display an error message.
-                Console.WriteLine($"Database initialization error: {ex.Message}");
-            }
+            // Proceed to create the database tables asynchronously, avoiding the blocking of the calling thread.
+            await CreateTablesAsync();
+            _isInitialised = true; // Indicate that initialisation has completed to prevent it from running again.
         }
-        public async Task CreateTablesAsync()
+
+        private async Task CreateTablesAsync()
         {
+            // Try to create the tables based on the specified model types.
             try
             {
-                // Create your database tables here
-                await _database.CreateTableAsync<Models.AlertType>();
-                await _database.CreateTableAsync<Models.OperationalTeamStatus>();
-                await _database.CreateTableAsync<Models.BuildingType>();
-                await _database.CreateTableAsync<Models.Continent>();
-                await _database.CreateTableAsync<Models.Equipment>();
-                await _database.CreateTableAsync<Models.OrderStatus>();
-                await _database.CreateTableAsync<Models.Organisation>();
-                await _database.CreateTableAsync<Models.position_statuses>();
-                await _database.CreateTableAsync<Models.Role>();
-                await _database.CreateTableAsync<Models.RoomType>();
-                await _database.CreateTableAsync<Models.Rota>();
-                await _database.CreateTableAsync<Models.Skill>();
-                await _database.CreateTableAsync<Models.SystemType>();
-                await _database.CreateTableAsync<Models.Expert>();
-
+                await _database.CreateTablesAsync(CreateFlags.None, new[] {
+                    typeof(Models.AlertType),
+                    typeof(Models.OperationalTeamStatus),
+                    typeof(Models.BuildingType),
+                    typeof(Models.Continent),
+                    typeof(Models.Equipment),
+                    typeof(Models.OrderStatus),
+                    typeof(Models.Organisation),
+                    typeof(Models.position_statuses),
+                    typeof(Models.Role),
+                    typeof(Models.RoomType),
+                    typeof(Models.Rota),
+                    typeof(Models.Skill),
+                    typeof(Models.SystemType),
+                    typeof(Models.Expert)
+                }).ConfigureAwait(false); // Using ConfigureAwait(false) to avoid capturing the synchronisation context and thus prevent deadlocks.
             }
             catch (Exception ex)
             {
-                // Handle any table creation errors here, e.g., log or display an error message.
-                Console.WriteLine($"Table creation error: {ex.Message}");
+                // Log the exception in a manner consistent with the application's logging strategy.
+                Console.WriteLine($"An error occurred while creating the database tables: {ex.Message}");
+                throw; // Re-throwing the exception to ensure the calling code can manage it effectively.
             }
         }
     }

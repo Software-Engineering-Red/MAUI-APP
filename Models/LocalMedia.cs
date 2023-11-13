f8e3924e5e39d26a4c04e6e0e74fb7f3374af110
Emesaye <k.pajak.pjx@gmail.com>
2023-11-13 19:10:52 +0000

    Added Model class for local media

---
+++ Models/LocalMedia.cs
@@ -0,0 +1,34 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UndacApp.Models
+{
+    public class LocalMedia : INotifyPropertyChanged
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        /*! <summary>
+         * Determine a private field 'name' to store the value of the 'Name' property.
+         * </summary> 
+         */
+        private string name;
+        public string Name
+        {
+            get => name;
+            set => Utils.SetProperty(ref name, value, this);
+        }
+        public string type;
+        public string Type
+        {
+            get => type;
+            set => Utils.SetProperty(ref type, value, this);
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
+}

db27b2ef492c374fe00099c8a855da670381b91d
Brandon <brandon.streetsmcleod@gmail.com>
2023-11-17 09:16:35 +0000

    updates based off comments

--- Models/OperationalTeam.cs
+++ Models/OperationalTeam.cs
@@ -1,7 +1,4 @@
-﻿using SQLite;
-using SQLiteNetExtensions.Attributes;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
--- Services/OperationalTeam/OperationalTeamService.cs
+++ Services/OperationalTeam/OperationalTeamService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using UndacApp.Models;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SQLite;
+﻿using UndacApp.Models;
 
 namespace UndacApp.Services
 {

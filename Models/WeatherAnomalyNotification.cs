4906c8982221369524adaa5f96695711ff4c4ac3
Stefan Humpelstetter <38726293+batchgott@users.noreply.github.com>
2023-11-19 19:46:17 +0000

    feature/MAUI#180 (#181)

---
+++ Models/WeatherAnomalyNotification.cs
@@ -0,0 +1,32 @@
+﻿using SQLite;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace UndacApp.Models
+{
+    public class WeatherAnomalyNotification : AModel
+    {
+
+        private string _message;
+        public string Message
+        {
+            get => _message;
+            set => SetField(ref _message, value);
+        }
+
+        private DateTime _createdAt;
+        public DateTime CreatedAt
+        {
+            get => _createdAt;
+            set => SetField(ref _createdAt, value);
+        }
+
+        private bool _cleared;
+        public bool Cleared
+        {
+            get => _cleared;
+            set => SetField(ref _cleared, value);
+        }
+    }
+}

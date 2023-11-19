448692b0282bf64c4e1e37b7e95274d221739466
Emily Herron <egretfulherron@gmail.com>
2023-11-19 22:17:20 +0000

    Added changes for User issue (#182)
    * Added changes for User issue
    
    * Added review changes

---
+++ Models/User.cs
@@ -0,0 +1,29 @@
+﻿using SQLite;
+using System.ComponentModel;
+
+namespace UndacApp.Models
+{
+    public class User : AModel
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => SetField(ref _name, value);
+        }
+
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set => SetField(ref _email, value);
+        }
+
+        private string _accessLevel;
+        public string AccessLevel
+        {
+            get => _accessLevel;
+            set => SetField(ref _accessLevel, value);
+        }
+    }
+}

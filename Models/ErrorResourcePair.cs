4bde8b3c936bc3ff37376899581c6175e3ffff21
Georgi Lefterov <46247795+GLefterov@users.noreply.github.com>
2023-11-21 07:59:00 +0200

    Added a new page and required viewModels

---
+++ Models/ErrorResourcePair.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UndacApp.Models
+{
+    public class ErrorResourcePair
+    {
+        public string Error { get; set; }
+        public string Resource { get; set; }
+
+        // Default constructor
+        public ErrorResourcePair()
+        {
+        }
+
+        // Overloaded constructor for initializing properties
+        public ErrorResourcePair(string error, string resource)
+        {
+            Error = error;
+            Resource = resource;
+        }
+
+        // Override ToString for easy debugging and display
+        public override string ToString()
+        {
+            return $"Error: {Error}, Resource: {Resource}";
+        }
+
+        // Other methods as needed...
+    }
+}

9e4784d56dc25f7fbf4231ff2c825cff8bcb9514
Stefan Humpelstetter <shumpelstetter@gmail.com>
2023-11-22 22:28:48 +0000

    refactor code

---
+++ Commands/ClearNotificationCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace UndacApp.Commands
+{
+    public class ClearNotificationCommand : ICommand
+    {
+        private readonly Action _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public ClearNotificationCommand(Action executeAction)
+        {
+            _execute = executeAction ?? throw new ArgumentNullException(nameof(executeAction));
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute?.Invoke();
+        }
+    }
+}
--- Views/WeatherAnomalyNotificationsPage.xaml.cs
+++ Views/WeatherAnomalyNotificationsPage.xaml.cs
@@ -1,92 +1,17 @@
 using System.Collections.ObjectModel;
 using UndacApp.Models;
 using UndacApp.Services;
+using UndacApp.ViewModels;
 
 
 namespace UndacApp.Views
 {
     public partial class WeatherAnomalyNotificationsPage : ContentPage
     {
-        ObservableCollection<WeatherAnomalyNotification> _currentNotifications = new ObservableCollection<WeatherAnomalyNotification>();
-        ObservableCollection<WeatherAnomalyNotification> _clearedNotifications = new ObservableCollection<WeatherAnomalyNotification>();
-        WeatherAnomalyNotification _selectedNotification;
-        IWeatherAnomalyNotificationService _weatherAnomalyNotificationService = new WeatherAnomalyNotificationService();
-
         public WeatherAnomalyNotificationsPage()
         {
             InitializeComponent();
-            LoadNotifications();
-        }
-
-        public async void LoadNotifications()
-        {
-            var notifications = await _weatherAnomalyNotificationService.GetAll();
-            CategorizeNotifications(notifications);
-            cv_currentNotifications.ItemsSource = _currentNotifications;
-            ltv_clearedNotifications.ItemsSource = _clearedNotifications;
-        }
-
-        public void CategorizeNotifications(IEnumerable<WeatherAnomalyNotification> notifications)
-        {
-            foreach (var notification in notifications)
-            {
-                if (notification.Cleared)
-                    _clearedNotifications.Add(notification);
-                else
-                    _currentNotifications.Add(notification);
-            }
-        }
-
-        public async void ReportNotificationButton_Clicked(object sender, EventArgs e)
-        {
-            if (string.IsNullOrEmpty(txe_weatherAnomalyNotification.Text))
-                return;
-
-            var notification = new WeatherAnomalyNotification
-            {
-                Message = txe_weatherAnomalyNotification.Text,
-                CreatedAt = DateTime.Now,
-                Cleared = false
-            };
-
-            await _weatherAnomalyNotificationService.Add(notification);
-            _currentNotifications.Add(notification);
-            txe_weatherAnomalyNotification.Text = "";
-        }
-
-        public void cv_currentNotifications_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            UpdateUIForSelectedNotification(e.CurrentSelection.FirstOrDefault() as WeatherAnomalyNotification);
-        }
-
-        public void UpdateUIForSelectedNotification(WeatherAnomalyNotification selectedNotification)
-        {
-            _selectedNotification = selectedNotification;
-            btnClear.IsVisible = _selectedNotification != null;
-        }
-
-        public async void ClearButton_Clicked(object sender, EventArgs e)
-        {
-            if (_selectedNotification == null)
-                return;
-
-            await UpdateNotificationStatus(_selectedNotification);
-
-            UpdateUIAfterClearingNotification();
-        }
-
-        public async Task UpdateNotificationStatus(WeatherAnomalyNotification notification)
-        {
-            notification.Cleared = true;
-            await _weatherAnomalyNotificationService.Update(notification);
-        }
-
-        public void UpdateUIAfterClearingNotification()
-        {
-            _currentNotifications.Remove(_selectedNotification);
-            _clearedNotifications.Add(_selectedNotification);
-            cv_currentNotifications.SelectedItem = null;
-            btnClear.IsVisible = false;
+            BindingContext = new WeatherAnomalyNotificationsViewModel();
         }
     }
 }

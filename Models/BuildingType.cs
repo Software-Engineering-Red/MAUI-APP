dba02a79bb8789917b952593a6d520c62a79988d
Calvin Donaldson <calvindonaldson1997313@gmail.com>
2023-11-24 12:25:16 +0000

    start of building type

--- Models/BuildingType.cs
+++ Models/BuildingType.cs
@@ -5,6 +5,40 @@
     /// </summary>
     public class BuildingType : AModel
     {
+        private int _id;
+        public int Id
+        {
+            get => _id;
+            set => SetField(ref _id, value);
+        }
 
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => SetField(ref _name, value);
+        }
+
+        private int _number;
+        public int Number
+        {
+            get => _number;
+            set => SetField(ref _number, value);
+        }
+
+        private string _type;
+        public string Type
+        {
+            get => _type;
+            set => SetField(ref _type, value);
+        }
+
+        private string _mapArea;
+        public string MapArea
+        {
+            get => _mapArea;
+            set => SetField(ref _mapArea, value); 
+        }
     }
 }
+

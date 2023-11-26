20732fd2311cb2c55f6e554d3d95e58505942e8f
Pannoniae <25565223+Pannoniae@users.noreply.github.com>
2023-11-26 20:43:38 +0000

    - add operation page

---
+++ Services/Operation/IOperationService.cs
@@ -0,0 +1,9 @@
+﻿using UndacApp.Models;
+
+namespace UndacApp.Services
+{
+    public interface IOperationService : IService<Operation>
+    {
+        Task<List<Operation>> GetOperationsByStatus(OperationStatus status);
+    }
+}
